e3c4cc32213e7c46051b1e159d425938cdd3290a
asd <asdzog@mail.ru>
2024-02-13 08:12:47 +0400

    add custom command to fill db

---
+++ users/management/commands/fill_payments_data.py
@@ -0,0 +1,19 @@
+from django.core.management import BaseCommand
+
+from courses.models import Lesson, Course
+from users.models import User, Payment
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        payment_to_fill = []
+
+        for user in User.objects.all():
+            payment_to_fill.append(Payment(user=user, lesson=Lesson.objects.get(pk=1),
+                                           payment_amount=1000, payment_method='cash'))
+        for user in User.objects.all():
+            payment_to_fill.append(Payment(user=user, course=Course.objects.get(pk=1),
+                                           payment_amount=20000, payment_method='transfer_to_account'))
+
+        Payment.objects.bulk_create(payment_to_fill)
