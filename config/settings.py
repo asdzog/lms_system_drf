24b0146ace0ef6894ac276c8a9aebd989b809e8a
asd <asdzog@mail.ru>
2024-02-21 16:45:18 +0400

    change method for requesting environment variables

--- config/settings.py
+++ config/settings.py
@@ -11,17 +11,18 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv(BASE_DIR / '.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6yz!=t8!bn))7q-0++y!vs(yq_jvcpi%14(9mqt+^q!tp87*kj'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
