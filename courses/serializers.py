13cfd4e5d7d521b505fc556332f682f51782c1be
asd <asdzog@mail.ru>
2024-02-21 16:45:44 +0400

    change CourseSerializer

--- courses/serializers.py
+++ courses/serializers.py
@@ -3,19 +3,20 @@ from rest_framework import serializers
 from courses.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
+    lesson = LessonSerializer(source='lesson_set', many=True, read_only=True)
 
     def get_lessons_count(self, obj):
         return Lesson.objects.filter(course=obj.id).count()
 
     class Meta:
         model = Course
-        fields = '__all__'
-
-
-class LessonSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Lesson
         fields = '__all__'
