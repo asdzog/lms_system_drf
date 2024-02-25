944d314ec26472368fe83a5ec937fcafe7ce4c66
asd <asdzog@mail.ru>
2024-02-25 07:36:27 +0400

    change permissions in Course and Lesson models

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 
 from courses.models import Course, Lesson
+from courses.permissions import IsModerator, IsOwner
 from courses.serializers import CourseSerializer, LessonSerializer
 
 
@@ -8,25 +10,44 @@ class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
 
+    def perform_create(self, serializer):
+        new_course = serializer.save()
+        new_course.owner = self.request.user
+        new_course.save()
+
+    def get_permissions(self):
+        if self.action in ('create', ):
+            self.permission_classes = [IsAuthenticated, ~IsModerator]
+        elif self.action in ('list', 'retrieve', 'update', 'partial_update'):
+            self.permission_classes = [IsAuthenticated, IsModerator | IsOwner]
+        elif self.action in ('destroy', ):
+            self.permission_classes = [IsAuthenticated, IsOwner]
+        return [permission() for permission in self.permission_classes]
+
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
