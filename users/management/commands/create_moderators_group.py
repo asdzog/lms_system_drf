6233102e111a59dd98c1f4566140dd8e4759e563
Aleksey Dzogiy <asdzog@mail.ru>
2024-02-28 19:39:07 +0400

    change custom command for creating moderators group

--- users/management/commands/create_moderators_group.py
+++ users/management/commands/create_moderators_group.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
-from users.models import User
+from users.models import Lesson, Course
 
 
 class Command(BaseCommand):
@@ -9,11 +9,18 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         moderators_group, created = Group.objects.get_or_create(name='Moderators')
 
-        content_type = ContentType.objects.get_for_model(User)
-        permissions = Permission.objects.filter(content_type=content_type)
+        lesson_content_type = ContentType.objects.get_for_model(Lesson)
+        course_content_type = ContentType.objects.get_for_model(Course)
 
-        moderators_permissions = []
-        for permission in permissions:
-            if permission.codename.startswith('change_') or permission.codename.startswith('view_'):
-                moderators_permissions.append(permission)
-        moderators_group.permissions.set(moderators_permissions)
+        lesson_view_permissions = Permission.objects.filter(content_type=lesson_content_type,
+                                                            codename__startswith='view_')
+        course_view_permissions = Permission.objects.filter(content_type=course_content_type,
+                                                            codename__startswith='view_')
+
+        lesson_change_permissions = Permission.objects.filter(content_type=lesson_content_type,
+                                                              codename__startswith='change_')
+        course_change_permissions = Permission.objects.filter(content_type=course_content_type,
+                                                              codename__startswith='change_')
+
+        moderator_permissions = lesson_view_permissions | course_view_permissions | lesson_change_permissions | course_change_permissions
+        moderators_group.permissions.set(moderator_permissions)
