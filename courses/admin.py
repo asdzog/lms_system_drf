ce8941aeda396dad0c84a1736314b734cc535487
Aleksey Dzogiy <asdzog@mail.ru>
2024-02-28 18:56:43 +0400

    edit admin panel (edit User, add Course and Lesson fields)

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from courses.models import Course, Lesson
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'course_name', 'course_description', 'owner')
+    list_filter = ('course_name', 'owner',)
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'lesson_name', 'lesson_description', 'video_url', 'course', 'owner')
+    list_filter = ('lesson_name', 'course', 'owner')
--- users/admin.py
+++ users/admin.py
@@ -4,4 +4,33 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+
+    list_display = ['email', 'first_name', 'last_name', 'is_superuser', 'is_staff']
+    exclude = ['username']
+    ordering = ['email']
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
