bc084d05e8a5f22d72f58757474465fe49d4caf5
asd <asdzog@mail.ru>
2024-03-03 02:57:11 +0400

    edit path to lessons list from 'lesson' to 'lessons'

--- courses/urls.py
+++ courses/urls.py
@@ -15,7 +15,7 @@ router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
     path('lesson/create/', LessonCreateAPIView.as_view(), name='create_lesson'),
-    path('lesson/', LessonListAPIView.as_view(), name='list_lesson'),
+    path('lessons/', LessonListAPIView.as_view(), name='list_lesson'),
     path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='get_lesson'),
     path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='update_lesson'),
     path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='delete_lesson'),
