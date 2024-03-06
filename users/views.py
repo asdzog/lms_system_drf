16f082a179b3e841c63374c9b2dd98d456fdb4cc
asd <asdzog@mail.ru>
2024-03-06 17:38:55 +0400

    add user registration endpoint

--- users/views.py
+++ users/views.py
@@ -1,9 +1,14 @@
-from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from users.models import Payment
-from users.serializers import PaymentSerializer
+from users.serializers import PaymentSerializer, UserSerializer
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
 
 
 class PaymentListAPIView(generics.ListAPIView):
