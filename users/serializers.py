75003438cd530336c737e2a9ab230a6581e4f3f9
asd <asdzog@mail.ru>
2024-03-10 07:44:45 +0400

    edit Payment serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from users.models import Payment, User
+from courses import services
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -15,6 +16,15 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class PaymentSerializer(serializers.ModelSerializer):
+    payment_url = serializers.SerializerMethodField(read_only=True)
+
+    @staticmethod
+    def get_stripe_url(instance: Payment):
+        product = services.create_stripe_product(instance.paid_course.course_name)
+        price_id = services.create_stripe_price(instance.paid_course.price, product['name'])
+        url, session_id = services.create_stripe_session(price_id)
+        return url, session_id
+
     class Meta:
         model = Payment
         fields = '__all__'
